9da27730ed039f4e1d61a68aa250c66f4a236b86
Andrey Emelianov <621077@mail.ru>
2024-04-18 17:16:13 +0300

    приступил к созданию ui компонентов, реализовал компонент кнопки UiButton

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import { authControllerGetSessionInfo } from "@/shared/api/generated";
 import { useQuery } from "@tanstack/react-query";
+import { UiButton } from "@/shared/ui/ui-button";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,12 @@ export function HomePage() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       {data?.email}
+      <UiButton variant="primary">prim</UiButton>
+      <UiButton variant="secondary">sec</UiButton>
+      <UiButton variant="outlined">out</UiButton>
+      <UiButton disabled variant="primary">
+        disabled
+      </UiButton>
     </main>
   );
 }
---
+++ src/shared/ui/ui-button.tsx
@@ -0,0 +1,27 @@
+import clsx from "clsx";
+import { ButtonHTMLAttributes } from "react";
+
+export type UiButtonVariant = "primary" | "secondary" | "outlined";
+export type UiButtonProps = {
+  variant: UiButtonVariant;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function UiButton({ className, variant, ...props }: UiButtonProps) {
+  return (
+    <button
+      {...props}
+      className={clsx(
+        className,
+        "px-4 h-10 rounded cursor-pointer flex gap-2 justify-center items-center",
+        {
+          primary:
+            "text-white bg-cyan-500 hover:bg-cyan-600 disabled:opacity-50 shadow shadow-cyan-600/40",
+          secondary:
+            "text-white bg-rose-500 hover:bg-rose-600 disabled:opacity-50 shadow shadow-rose-600/40",
+          outlined:
+            "border border-slate-300 hover:bg-slate-500 hover:border-slate-100 disabled:opacity-50",
+        }[variant],
+      )}
+    />
+  );
+}
